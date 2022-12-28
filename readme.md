9c426f54e737d7fa058a9452fff5e336e35f9162
Chris Pak <64233202+cpak10@users.noreply.github.com>
2022-12-27 19:35:23 -0800

    Add MySQL commands to subset_data.py.

--- readme.md
+++ readme.md
@@ -3,8 +3,14 @@ This repository contains tools to subset and analyze the opioid transactions in 
 can be found in the following file: requirements.txt
 
 ## Data
-Data for opioid transactions in the United States from 2006 to 2014 is publicly available through court order. 
+Data for opioid transactions in the United States from 2006 to 2014 is publicly available through court order ("ARCOS"). 
 This data can be found at the following link: https://www.slcg.com/opioid-data/
 
-The full transaction data is approximately 500 million observations and over 200 GB in size. Use the subset_data.py script to 
-subset the full data into manageable portions. Use the create_table.sql to set up the table in a MySQL server.
+The full transaction data is approximately 500 million observations and over 200 GB in size.
+
+## Deployment
+* subset_data.py: 
+This script assumes that the ARCOS data referenced above is downloaded and a connection to a MySQL server is ready. 
+The script will filter the ARCOS data and commit the data into a new table in MySQL. 
+The file path for the ARCOS data will need to be updated based on local save location. 
+Additionally, the batch size for the pandas loop may need to be updated based on local RAM availabilities.
--- subset_data.py
+++ subset_data.py
@@ -1,20 +1,70 @@
 import pandas as pd
 import numpy as np
+import mysql.connector
+from mysql.connector import errorcode
 
 # define the filters
 city = "BEVERLY HILLS"
 state = "CA"
 county = "LOS ANGELES"
+table_name = "arcos_ca_los_angeles_beverly_hills"
+
+# define mysql database and tables
+db_name = "opioids"
+table = f"""
+    create table {table_name}(
+        id int not null,
+        reporter_dea_no varchar(256), reporter_bus_act varchar(256), reporter_name varchar(256), 
+        reporter_addl_co_info varchar(256), reporter_address1 varchar(256), reporter_address2 varchar(256),
+        reporter_city varchar(256), reporter_state varchar(256), reporter_zip varchar(256), reporter_county varchar(256),
+        buyer_dea_no varchar(256), buyer_bus_act varchar(256), buyer_name varchar(256),
+        buyer_addl_co_info varchar(256), buyer_address1 varchar(256), buyer_address2 varchar(256),
+        buyer_city varchar(256), buyer_state varchar(256), buyer_zip varchar(256), buyer_county varchar(256),
+        transaction_code varchar(256), drug_code varchar(256), ndc_no varchar(256), drug_name varchar(256),
+        quantity float(2), unit varchar(256), action_indicator varchar(256), order_form_no varchar(256),
+        correction_no varchar(256), strength varchar(256), transaction_date varchar(256), calc_base_wt_in_gm float(2),
+        dosage_unit varchar(256), transaction_id varchar(256), product_name varchar(256), ingredient_name varchar(256),
+        measure varchar(256), mme_conversion_factor float(2), combined_labeler_name varchar(256), 
+        revised_company_name varchar(256), reporter_family varchar(256), dos_str varchar(256), 
+        primary key (id)
+    )
+"""
 
 # ignore the error from unstringable values in the data
 np.seterr(invalid = "ignore")
 
-# set up the subset dataset
-arcos_sub = pd.DataFrame()
+# set up mysql connection
+cnx = mysql.connector.connect(user = "USER", password = "PASSWORD")
+cursor = cnx.cursor()
+
+# check for database and create database if does not exist
+def create_database(cursor):
+    try:
+        cursor.execute(f"create database {db_name}")
+    except mysql.connector.Error as err:
+        print(f"ERROR: Failed creating database: {err}")
+        exit(1)
+
+try:
+    cursor.execute(f"use {db_name}")
+except mysql.connector.Error as err:
+    print(f"\nNOTE: Database {db_name} does not exists.")
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print(f"NOTE: Database {db_name} created successfully.")
+        cnx.database = db_name
+    else:
+        print(err)
+        exit(1)
+
+# drop table and create fresh table
+cursor.execute(f"drop table if exists {table_name}")
+cursor.execute(table)
 
 # loop through the full data in manageable chunks
 n_chunk = 0
-for chunk in pd.read_csv("C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\ARCOS US 14 Drugs Nov 2020.csv", sep = "|", iterator = True, chunksize = 20000000,
+n_observations = 0
+for chunk in pd.read_csv("C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\ARCOS US 14 Drugs Nov 2020.csv", sep = "|", iterator = True, chunksize = 10000000,
     # set the datatypes for each column explicitly
     dtype = {
         "REPORTER_DEA_NO": "object", "REPORTER_BUS_ACT": "object", "REPORTER_NAME": "object",
@@ -25,17 +75,36 @@ for chunk in pd.read_csv("C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\ARC
         "BUYER_CITY": "object", "BUYER_STATE": "object", "BUYER_ZIP": "object", "BUYER_COUNTY": "object",
         "TRANSACTION_CODE": "object", "DRUG_CODE": "object", "NDC_NO": "object", "DRUG_NAME": "object",
         "QUANTITY": "float64", "UNIT": "object", "ACTION_INDICATOR": "object", "ORDER_FORM_NO": "object",
-        "CORRECTION_NO": "object", "STRENGTH": "float64", "TRANSACTION_DATE": "object", "CALC_BASE_WT_IN_GM": "float64",
-        "DOSAGE_UNIT": "float64", "TRANSACTION_ID": "object", "Product_Name": "object", "Ingredient_Name": "object", 
+        "CORRECTION_NO": "object", "STRENGTH": "object", "TRANSACTION_DATE": "object", "CALC_BASE_WT_IN_GM": "float64",
+        "DOSAGE_UNIT": "object", "TRANSACTION_ID": "object", "Product_Name": "object", "Ingredient_Name": "object", 
         "Measure": "object", "MME_Conversion_Factor": "float64", "Combined_Labeler_Name": "object", 
-        "Revised_Company_Name": "object", "Reporter_family": "object", "dos_str": "float64"
+        "Revised_Company_Name": "object", "Reporter_family": "object", "dos_str": "object"
     }
 ):
     chunk_sub = chunk[(chunk["BUYER_CITY"] == city) & (chunk["BUYER_STATE"] == state) & (chunk["BUYER_COUNTY"] == county)]
-    arcos_sub = pd.concat([arcos_sub, chunk_sub], ignore_index = True)
-    n_chunk = n_chunk + 1
-    print(f"\nNOTE: Processed {n_chunk * 20} million observations")
-    print(f"NOTE: Subset is {arcos_sub.shape[0]} observations")
+    if chunk_sub[["CALC_BASE_WT_IN_GM", "QUANTITY", "MME_Conversion_Factor"]].isna().any().any() == True:
+        print("\nERROR: Missing values in numeric columns")
+        exit(1)
+    chunk_sub_fill_missing = chunk_sub.fillna("")
+    for i, row in chunk_sub_fill_missing.iterrows():
+        row_tuple = tuple(row)
+        row_add_index = (n_observations,) + row_tuple
+        sql = f"""insert into {table_name} values(
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+            %s, %s, %s
+        )"""
+        cursor.execute(sql, tuple(row_add_index))
+        cnx.commit()
+        n_observations += 1
 
-# send the subset to csv
-arcos_sub.to_csv(f"C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\ARCOS_{state}_{county}_{city}.csv", sep = "|")
+    n_chunk += 1
+    print(f"\nNOTE: Processed {n_chunk * 10} million observations")
+    print(f"NOTE: Added {chunk_sub.shape[0]} observations to table")
+    print(f"NOTE: Table is {n_observations} observations")
+
+# closing mysql connection
+cursor.close()
+cnx.close()
